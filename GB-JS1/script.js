59d7f66cc3c3cb29dbb7d720629a6c0795c6004d
ak <a.kozlov@studioratio.ru>
2018-06-04 01:07:06 +0500

    JS1 Lesson 1

---
+++ GB-JS1/script.js
@@ -0,0 +1,19 @@
+alert('Задание 1');
+var tempCel = parseInt(prompt('Введите температуру в градусах Цельсия'));
+if (isNaN(tempCel)) {
+    alert('Вы ввели не число!')
+} else {
+    var tempFar = (9/5)*tempCel+32;
+    alert('Температура в градусах Фаренгейта равна: ' + tempFar);
+}
+
+alert ('Задание 3');
+var name = 'Василий';
+var admin = name;
+alert(admin);
+
+alert('Задание 4');
+alert(1000+'108');
+//->1000108.
+
+
