14be691c33e264fef19fd24e1023e683d86ecd38
Andrey Kozlov <kozlovaa1@yandex.ru>
2018-06-21 17:58:56 +0300

    Частично доработано задание 3 урока 4

--- GB-JS1/game/gameCode.js
+++ GB-JS1/game/gameCode.js
@@ -1,20 +1,48 @@
 var event, ok;
-var i = 0;
-do {
-    ok = false;
-    event = +prompt(game[i].question + game[i].answers + '-1 - Выход из игры');
-    if (event == -1) {
-        break;
+
+function startGame() {
+    var money = 0;
+    var answerCorrect = 0;
+    var gain = 0; // выигрыш
+    var hold = 0; // несгораемая сумма
+    for (var i = 0; i < game.length; i++) {
+        if (i > 0 && confirm('Ок - продолжить игру. Отмена - забрать деньги и закончить игру.')) {
+            alert('Поздравляем! Вы выиграли ' + money + ' монет!');
+            break;
+        }
+        do {
+            ok = false;
+            event = +prompt(game[i].question + game[i].answers + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(event);
+            }
+        } while (!ok);
+        if (event == game[i].correct) {
+            answerCorrect++;
+            money = game[i].money;
+            if ((i + 1) % 5 == 0) {
+                hold = money;
+            } else {
+                break;
+            }
+        }
+        else {
+            gain = hold;
+            if (gain == 0) {
+                alert("Это неправильный ответ! Все ваши деньги сгорают. Игра окончена.")
+            }
+            else {
+                alert("Это неправильный ответ! Сумма вашего выигрыша - " + gain + " монет. Игра окончена.")
+            }
+        }
     }
-    else {
-        ok = isAnswer(event);
-    }
-} while (!ok);
-if(event == game[i].correct) {
-    answerCorrect++;
+
+    alert('Спасибо за игру');
 }
 
-alert('Спасибо за игру');
 
 //------------------------------------------
 function isAnswer(event) {
@@ -27,5 +55,5 @@ function isAnswer(event) {
         return false;
     }
     return true;
-    
+
 }
