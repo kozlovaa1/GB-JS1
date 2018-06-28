d6f1b4258cac7a274a01ca131808e0481cc1ac93
ak <a.kozlov@studioratio.ru>
2018-06-28 23:19:00 +0500

    Частично выполнил ДЗ урок 7.

---
+++ GB-JS1/cart/script.js
@@ -0,0 +1,21 @@
+var goods = [
+    {
+        title: 'Товар 1',
+        price: 1000,
+        img: '../slider/img/1.jpg'
+    },
+    {
+        title: 'Товар 2',
+        price: 2000,
+        img: '../slider/img/2.jpg'
+    },
+    {
+        title: 'Товар 3',
+        price: 3000,
+        img: '../slider/img/3.jpg'
+    }
+];
+
+function addToCart() {
+    
+}
--- GB-JS1/slider/script.js
+++ GB-JS1/slider/script.js
@@ -13,5 +13,32 @@ function changeBigPicture(eventObj){
     var imageDomElement = document.createElement("img");
     imageDomElement.src = src;
     appDiv.appendChild(imageDomElement);
+    appDiv.onerror = function() {
+        alert("Увеличенное изображение не найдено!")
+    }
 }
 window.onload = init;
+
+var btn_prev = document.getElementsByClassName('prev');
+var btn_next = document.getElementsByClassName('next');
+var images = document.getElementsByClassName('galleryItem');
+var i=0;
+
+btn_prev.onclick = function () {
+    images[i].style.display = 'none';
+    i--;
+
+    if(i == -1) {
+        i = images.length-1;
+    }
+    images[i].style.display = 'block';
+}
+btn_next.onclick = function () {
+    images[i].style.display = 'none';
+    i++;
+
+    if(i >= images.length) {
+        i = 0;
+    }
+    images[i].style.display = 'block';
+}
