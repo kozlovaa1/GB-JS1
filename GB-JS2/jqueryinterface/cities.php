3eac52e828df5e18714d0d25834cbc49b15e7bc3
ak <a.kozlov@studioratio.ru>
2018-07-15 17:28:04 +0500

    ДЗ урок 4

--- GB-JS2/jqueryinterface/cities.php
+++ GB-JS2/jqueryinterface/cities.php
@@ -1,5 +1,5 @@
 <?php
-$data = [["city" => "Анадырь"], ["city" => "Рязань"], ["city" => "Южно-Сахалинск"]];
+$data = [["city" => "Анадырь"], ["city" => "Рязань"], ["city" => "Казань"]];
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 echo json_encode($data);
