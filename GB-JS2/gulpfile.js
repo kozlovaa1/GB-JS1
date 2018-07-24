e66bcf4364f616266e538ba9a0c59b3778cd42e6
ak <a.kozlov@studioratio.ru>
2018-07-25 01:02:29 +0500

    Настройка gulp

--- GB-JS2/gulpfile.js
+++ GB-JS2/gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
+var dest = require('gulp-dest');
+var useref = require('gulp-useref');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
 
-gulp.task('task-name', function() {
+gulp.task('task-name', function () {
 // код
 });
 
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync({
         server: {
             baseDir: 'app'
@@ -13,31 +19,38 @@ gulp.task('browserSync', function() {
     })
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss') // Получаем все файлы с окончанием .scss в папке app/scss и дочерних директориях
         .pipe(sass())
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-// Обновляем браузер при любых изменениях в HTML или JS
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass'])
+    // Обновляем браузер при любых изменениях в HTML или JS
+        .pipe('app/*.html', browserSync.reload)
+        .pipe('app/js/**/*.js', browserSync.reload)
 });
 
-var useref = require('gulp-useref');
-
-gulp.task('useref', function(){
+gulp.task('useref', function () {
     var assets = useref.assets();
     return gulp.src('app/*.html')
         .pipe(assets)
         .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('babel', function () {
+    return gulp.src('app/js/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('app/js'))
 });
 
 
