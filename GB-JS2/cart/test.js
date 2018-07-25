622285fdacb51711e953196bade0fadffcfb6fac
ak <a.kozlov@studioratio.ru>
2018-07-26 00:08:39 +0500

    Настройка тестирования

---
+++ GB-JS2/cart/test.js
@@ -0,0 +1,13 @@
+// mocha.setup('bdd');
+// let assert = chai.assert;
+// mocha.run();
+
+describe('Создание корзины', () => {
+    /*let cartResult;
+    before(async () => {
+        cartResult = await createCart().then(res = res.json);
+    });*/
+    it('корзина для текущего пользователя создана', () => {
+        assert.isDefined(11);
+    })
+})
