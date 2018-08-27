911192c2543e026d5b5e494a6fffec040f74ce39
Andrey Kozlov <kozlovaa1@yandex.ru>
2018-08-27 15:16:21 +0300

    ДЗ 2

--- GB-PHP2/lesson2/index.php
+++ GB-PHP2/lesson2/index.php
@@ -1,46 +1,147 @@
 <?php
+
 /*1. Создать структуру классов ведения товарной номенклатуры.
 а) Есть абстрактный товар.
 б) Есть цифровой товар, штучный физический товар и товар на вес.
 в) У каждого есть метод подсчета финальной стоимости.
 г) У цифрового товара стоимость постоянная – дешевле штучного товара в два раза. У штучного товара обычная стоимость, у весового – в зависимости от продаваемого количества в килограммах. У всех формируется в конечном итоге доход с продаж.
 д) Что можно вынести в абстрактный класс, наследование?*/
+
 abstract class Product
 {
-    public $id;
+    protected $id;
+    protected $name;
+    protected $description;
+    protected $price;
 
-
-    function __construct($id, $name)
+    protected function __construct($id, $name, $description, $price)
     {
         $this->id = $id;
         $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
     }
 
+    abstract protected function getPrice();
 
-    public function showName()
+    abstract protected function getRevenue();
+
+
+}
+
+trait Revenue
+{
+    public function getRevenue()
     {
-        echo '<h1>' . $this->name . '</h1>';
+        return ($this->getPrice()) * 0.05;
     }
 }
 
 class ProductDigital extends Product
 {
-    public $price;
-    public $description;
+    use Revenue;
 
-    function __construct($id, $name, $price, $description)
+    public function getPrice()
     {
-        parent:: __construct($id, $name);
-        $this->price = $price;
-        $this->description = $description;
+        return ($this->price) / 2;
     }
 
-
-    public function getValue()
+    function __construct($id, $name, $description, $price)
     {
-        echo '<p>' . $this->price . ' руб.</p><p>Наименование: ' . $this->name . '</p><p>' . $this->description . '</p>';
+        parent:: __construct($id, $name, $description, $price);
     }
 }
 
+class ProductPiece extends Product
+{
+    use Revenue;
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    function __construct($id, $name, $description, $price)
+    {
+        parent:: __construct($id, $name, $description, $price);
+    }
+}
+
+class ProductByWeight extends Product
+{
+    use Revenue;
+
+    private $weight;
+
+    function __construct($id, $name, $description, $price, $weight)
+    {
+        parent:: __construct($id, $name, $description, $price);
+        $this->weight = $weight;
+    }
+
+    public function getPrice()
+    {
+        return ($this->price) * ($this->weight);
+    }
+}
+
+$item1 = new ProductDigital(1, 'Товар 1', 'Описание', 100);
+echo $item1->getPrice() . '<br>';
+
+$item2 = new ProductPiece(2, 'Товар 2', 'Описание', 100);
+echo $item2->getPrice() . '<br>';
+
+$item3 = new ProductByWeight(3, 'Товар 3', 'Описание', 100, 5);
+echo $item3->getPrice() . '<br>';
+
+$revenue1 = $item1->getRevenue();
+$revenue2 = $item2->getRevenue();
+$revenue3 = $item3->getRevenue();
+
+echo "Доход 1: $revenue1 <br> Доход 2: $revenue2<br> Доход 3: $revenue3";
 
 /*    2. *Реализовать паттерн Singleton при помощи traits.*/
+
+trait MyTrait
+{
+    public function myFunc()
+    {
+        return 2 + 2;
+    }
+}
+
+class Singleton
+{
+    private static $instance; // Экземпляр объекта
+
+// Защищаем от создания через new Singleton
+    private function __construct()
+    { /* ... @return Singleton */
+    }
+
+// Защищаем от создания через клонирование
+    private function __clone()
+    { /* ... @return Singleton */
+    }
+
+// Защищаем от создания через unserialize
+    private function __wakeup()
+    { /* ... @return Singleton */
+    }
+
+// Возвращает единственный экземпляр класса. @return Singleton
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function doAction()
+    {
+    }
+}
+
+/* Применение*/
+Singleton::getInstance()->doAction();
