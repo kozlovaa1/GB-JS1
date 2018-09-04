caf19e982eeb39c7485e809e66da272600f4b1bf
ak <a.kozlov@studioratio.ru>
2018-09-04 23:00:23 +0500

    ДЗ 4.

--- GB-PHP2/lesson4/models/db_goods.php
+++ GB-PHP2/lesson4/models/db_goods.php
@@ -1,31 +1,33 @@
 <?php
 require_once "db.php";
-function goods_new($link, $name, $description, $src, $small_src, $price){
+function goods_new($link, $name, $description, $src, $small_src, $price)
+{
 
     $t = "INSERT INTO goods (name, description, src, small_src, price) VALUES ('%s','%s','%s','%s','%s')";
 
-    $query = sprintf($t, mysqli_real_escape_string($link, $name),mysqli_real_escape_string($link, $description),mysqli_real_escape_string($link, $src),mysqli_real_escape_string($link, $small_src),mysqli_real_escape_string($link, $price));
+    $query = sprintf($t, mysqli_real_escape_string($link, $name), mysqli_real_escape_string($link, $description), mysqli_real_escape_string($link, $src), mysqli_real_escape_string($link, $small_src), mysqli_real_escape_string($link, $price));
 
     $result = mysqli_query($link, $query);
 
-    if(!$result){
+    if (!$result) {
         die(mysqli_error($link));
     }
 
     return true;
 }
 
-function goods_all($link){
-    $query = "SELECT * FROM goods order by id desc";
+function goods_all($link)
+{
+    $query = "SELECT * FROM goods order by id asc";
     $result = mysqli_query($link, $query);
 
-    if(!$result)
+    if (!$result)
         die(mysqli_error($link));
 
     $n = mysqli_num_rows($result);
     $goods = array();
 
-    for($i = 0; $i < $n; $i++){
+    for ($i = 0; $i < $n; $i++) {
         $row = mysqli_fetch_assoc($result);
         $goods[] = $row;
     }
@@ -33,11 +35,24 @@ function goods_all($link){
     return $goods;
 }
 
-function goods_get($link, $id){
-    $query = sprintf("SELECT * FROM goods where id=%d",(int)$id);
+function goods_ajax($link)
+{
+    $countView = (int)$_POST['count_add'];  // количество записей, получаемых за один раз
+    $startIndex = (int)$_POST['count_show']; // с какой записи начать выборку
+
+    $sql = mysqli_query($link,"SELECT * FROM `tbl_news` LIMIT $startIndex, $countView") or die(mysqli_error());
+    $newsData = array();
+    while ($result = mysqli_fetch_array($sql, MYSQLI_ASSOC)) {
+        $newsData[] = $result;
+    }
+}
+
+function goods_get($link, $id)
+{
+    $query = sprintf("SELECT * FROM goods where id=%d", (int)$id);
     $result = mysqli_query($link, $query);
 
-    if(!$result)
+    if (!$result)
         die(mysqli_error($link));
 
     $good = mysqli_fetch_assoc($result);
@@ -45,31 +60,33 @@ function goods_get($link, $id){
     return $good;
 }
 
-function goods_delete($link, $id){
+function goods_delete($link, $id)
+{
     $id = (int)$id;
 
-    if($id == 0)
+    if ($id == 0)
         return false;
 
     $query = sprintf("DELETE FROM goods where id='%d'", $id);
     $result = mysqli_query($link, $query);
 
-    if(!$result)
+    if (!$result)
         die(mysqli_error($link));
 
     return mysqli_affected_rows($link);
 }
 
-function goods_edit($link, $id, $name, $description, $src, $small_src, $price){
+function goods_edit($link, $id, $name, $description, $src, $small_src, $price)
+{
     $id = (int)$id;
 
     $sql = "UPDATE goods SET name='%s',description='%s',src='%s',small_src='%s',price='%s' WHERE id='%d'";
 
-    $query = sprintf($sql, mysqli_real_escape_string($link, $name),mysqli_real_escape_string($link, $description),mysqli_real_escape_string($link, $src),mysqli_real_escape_string($link, $small_src),mysqli_real_escape_string($link, $price),$id);
+    $query = sprintf($sql, mysqli_real_escape_string($link, $name), mysqli_real_escape_string($link, $description), mysqli_real_escape_string($link, $src), mysqli_real_escape_string($link, $small_src), mysqli_real_escape_string($link, $price), $id);
 
     $result = mysqli_query($link, $query);
 
-    if(!$result)
+    if (!$result)
         die(mysqli_error($link));
 
     return mysqli_affected_rows($link);
--- GB-PHP2/lesson4/public/catalog.php
+++ GB-PHP2/lesson4/public/catalog.php
@@ -8,26 +8,51 @@ include "../templates/header.php"; ?>
         <li class="breadcrumb-item active" aria-current="page">Каталог</li>
     </ol>
 </nav>
-        <h1>Каталог товаров</h1>
-        <hr>
+<h1>Каталог товаров</h1>
+<hr>
 
-        <?php
-        $goods = goods_all($link);
-        if($goods){
-            foreach ($goods as $good){?>
-                <div class="item">
-                    <a href="item.php?id=<?=$good['id']?>"><img src="<?=$good['small_src']?>" alt="<?=$good['name']?>" title="<?=$good['name']?>"></a>
-                    <h3 class="item-name"><a href="item.php?id=<?=$good['id']?>"><?=$good['name']?></a></h3>
-                    <p class="price"><?=$good['price']?>р.</p>
-                    <p class="add-to-basket"><a href="#" title="Добавить в корзину">Купить</a></p>
-                </div>
-            <?}
-        }
+<?php
+$goods = goods_all($link);
+if ($goods) {
+    foreach ($goods as $good) {
         ?>
-    </div>
-    <footer>
-        <? include "../templates/footer.php"; ?>
-    </footer>
+        <div class="item">
+            <a href="item.php?id=<?= $good['id'] ?>"><img src="<?= $good['small_src'] ?>" alt="<?= $good['name'] ?>"
+                                                          title="<?= $good['name'] ?>"></a>
+            <h3 class="item-name"><a href="item.php?id=<?= $good['id'] ?>"><?= $good['name'] ?></a></h3>
+            <p class="price"><?= $good['price'] ?>р.</p>
+            <p class="add-to-basket"><a href="#" title="Добавить в корзину">Купить</a></p>
+        </div>
+    <?
+    }
+}
+
+if (empty($newsData)) {
+// если новостей нет
+    echo json_encode(array(
+        'result' => 'finish'
+    ));
+} else {
+// если новости получили из базы, то сформируем html элементы
+// и отдадим их клиенту
+    $html = "";
+    foreach ($newsData as $oneNews) {
+        $html .= "
+<div>
+    <b>{$oneNews['title']}</b>
+    <p>{$oneNews['small_text']}</p>
+</div>
+";
+    }
+    echo json_encode(array(
+        'result' => 'success',
+        'html' => $html
+    ));
+} ?>
+</div>
+<footer>
+    <? include "../templates/footer.php"; ?>
+</footer>
 </div>
 </body>
 </html>
