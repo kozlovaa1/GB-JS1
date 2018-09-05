06fb4a33d9f93ca9a85b95ddcb7716b48f07c230
Andrey Kozlov <kozlovaa1@yandex.ru>
2018-09-05 17:05:21 +0300

    ДЗ 4. ДЗ 5 заготовка

--- GB-PHP2/lesson4/public/catalog.php
+++ GB-PHP2/lesson4/public/catalog.php
@@ -2,57 +2,56 @@
 include_once "../models/db_goods.php";
 CONST TITLE = 'Магазинчик волшебства';
 include "../templates/header.php"; ?>
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.php">Главная</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Каталог</li>
-    </ol>
-</nav>
-<h1>Каталог товаров</h1>
-<hr>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Главная</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Каталог</li>
+        </ol>
+    </nav>
+    <h1>Каталог товаров</h1>
+    <div class="row">
 
-<?php
-$goods = goods_all($link);
-if ($goods) {
-    foreach ($goods as $good) {
-        ?>
-        <div class="item">
-            <a href="item.php?id=<?= $good['id'] ?>"><img src="<?= $good['small_src'] ?>" alt="<?= $good['name'] ?>"
-                                                          title="<?= $good['name'] ?>"></a>
-            <h3 class="item-name"><a href="item.php?id=<?= $good['id'] ?>"><?= $good['name'] ?></a></h3>
-            <p class="price"><?= $good['price'] ?>р.</p>
-            <p class="add-to-basket"><a href="#" title="Добавить в корзину">Купить</a></p>
-        </div>
-    <?
-    }
-}
+        <?php
+        $goods = goods_all($link);
+        if ($goods) {
+            foreach ($goods as $good) {
+                ?>
+                <div class="item">
+                    <a href="item.php?id=<?= $good['id'] ?>"><img src="<?= $good['small_src'] ?>"
+                                                                  alt="<?= $good['name'] ?>"
+                                                                  title="<?= $good['name'] ?>"></a>
+                    <h3 class="item-name"><a href="item.php?id=<?= $good['id'] ?>"><?= $good['name'] ?></a></h3>
+                    <p class="price"><?= $good['price'] ?>р.</p>
+                    <p class="add-to-basket"><a href="#" title="Добавить в корзину">Купить</a></p>
+                </div>
+                <?
+            }
+        }
 
-if (empty($newsData)) {
+        if (empty($goods)) {
 // если новостей нет
-    echo json_encode(array(
-        'result' => 'finish'
-    ));
-} else {
+            echo json_encode(array(
+                'result' => 'finish'
+            ));
+        } else {
 // если новости получили из базы, то сформируем html элементы
 // и отдадим их клиенту
-    $html = "";
-    foreach ($newsData as $oneNews) {
-        $html .= "
-<div>
-    <b>{$oneNews['title']}</b>
-    <p>{$oneNews['small_text']}</p>
-</div>
-";
-    }
-    echo json_encode(array(
-        'result' => 'success',
-        'html' => $html
-    ));
-} ?>
-</div>
-<footer>
-    <? include "../templates/footer.php"; ?>
-</footer>
-</div>
-</body>
-</html>
+            $html = "";
+            foreach ($goods as $good) {
+                $html .= "
+                <div class=\"item\">
+                    <a href=\"item.php?id={$good['id']}\"><img src=\"{$good['small_src']}\" alt=\"{$good['name']}\"
+                                                                  title=\"{$good['name']}\"></a>
+                    <h3 class=\"item-name\"><a href=\"item.php?id={$good['id']}\">{$good['name']}</a></h3>
+                    <p class=\"price\">{$good['price']}р.</p>
+                    <p class=\"add-to-basket\"><a href=\"#\" title=\"Добавить в корзину\">Купить</a></p>
+                </div>
+            ";
+            }
+            echo json_encode(array(
+                'result' => 'success',
+                'html' => $html
+            ));
+        } ?>
+    </div>
+<? include "../templates/footer.php"; ?>
